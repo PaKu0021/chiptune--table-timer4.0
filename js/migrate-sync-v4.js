31037808ab693cb11945e147f6eac96e07ee9505
PaKu0021 <piaojunyi0021@gmail.com>
2026-07-26 15:30:59 +0900

    安全写回历史人民币付款 v4.0.43

--- js/migrate-sync-v4.js
+++ js/migrate-sync-v4.js
@@ -1,4 +1,4 @@
-﻿import { db } from "./firebase.js?v=4.0.42";
+﻿import { db } from "./firebase.js?v=4.0.43";
 import {
   doc,
   getDoc,
--- js/today-bill.js
+++ js/today-bill.js
@@ -1,10 +1,10 @@
 ﻿import { doc, onSnapshot, collection, setDoc } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-import { setStateBaseline, saveStateSafely, installConnectionGuard, setSyncStatus, loadLocalState, reconcileCloudState, flushPending, loadLocalRecords, mergeRecordLists, saveRecordSafely, subscribeAllRecords, emergencySaveRecord, emergencySaveState } from "./safe-state.js?v=4.0.42";
-import { encodeGroupDocumentId, ensureGroups } from "./group-model.js?v=4.0.42";
-import { dateKey, getCurrentBusinessDate, getRecordBusinessDate, getRecordTimestamp, RMB_PER_JPY, repairRecordPaymentAmounts } from "./business-day.js?v=4.0.42";
+import { setStateBaseline, saveStateSafely, installConnectionGuard, setSyncStatus, loadLocalState, reconcileCloudState, flushPending, loadLocalRecords, mergeRecordLists, saveRecordSafely, subscribeAllRecords, emergencySaveRecord, emergencySaveState } from "./safe-state.js?v=4.0.43";
+import { encodeGroupDocumentId, ensureGroups } from "./group-model.js?v=4.0.43";
+import { dateKey, getCurrentBusinessDate, getRecordBusinessDate, getRecordTimestamp, RMB_PER_JPY, repairRecordPaymentAmounts } from "./business-day.js?v=4.0.43";
 
 
-import { db } from "./firebase.js?v=4.0.42";
+import { db } from "./firebase.js?v=4.0.43";
 
 const ref = doc(db, "shop", "main");
 const recordsRef = collection(db, "records");
